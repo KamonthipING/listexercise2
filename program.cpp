bca2ebc8cbb21794e010e14a1a4585a3583bf9de
drkencosh <drkencosh@gmail.com>
2014-08-15 14:47:51 +0700

    Initial 3 files

---
+++ program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "list.h"
+using namespace std;
+
+void main()
+{
+	//Sample Code
+	List mylist;
+	mylist.pushToHead('k');
+	mylist.pushToHead('e');
+	mylist.pushToHead('n');
+	mylist.print();
+
+	//TO DO! Complete the functions, then write a program to test them.
+	//Adapt your code so that your list can store data other than characters - how about a template?
+	
+}
